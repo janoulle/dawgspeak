c64d026d11b45e427d652aabc5173af8db478211
Mouseion <cbxcel@gmail.com>
2014-03-22 13:30:22 -0600

    Update dbtest.php
    This comment has been identified as either hateful, hurtful or discriminatory as part of a project to #CleanUpGitHub. This pull request was made by a human. For more info, please check EthicalCode’s website at www.ethicalco.de/cleanupgh

--- classes/testers/dbtest.php
+++ classes/testers/dbtest.php
@@ -3,7 +3,7 @@ require_once("../helpers/db.php");
 require_once("../helpers/word.php");
 
 $db = new DB();
-$words = $db->getDefinitions("cunt");
+$words = $db->getDefinitions("crew");
 print_r($words);
 $words = $db->getDefinitions("12");
 print_r($words);
